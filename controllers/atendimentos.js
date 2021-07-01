a3a5313d895a57fb4a8049ccc1965a9558200a0a
dvargas42 <dvargas.eng@gmail.com>
2021-07-01 19:11:54 -0300

    feat: create atendimentos post endpoint

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
   app.get('/atendimentos', (request, response) => {
     response.send('Você está na rota de atendimentos e está realizando um GET')
   });
+
+  app.post('/atendimentos', (request, response) => {
+    console.log(request.body)
+    response.send('Você está na rota de atendimento e está realizando um POST')
+  })
 }
