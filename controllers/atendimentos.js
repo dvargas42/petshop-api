f2f26cb022b7f15c5fd180d713a899407e84bd00
dvargas42 <dvargas.eng@gmail.com>
2021-07-02 19:32:23 -0300

    feat: create data persitency

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
   app.get('/atendimentos', (request, response) => {
     response.send('Você está na rota de atendimentos e está realizando um GET')
   });
 
   app.post('/atendimentos', (request, response) => {
-    console.log(request.body)
-    response.send('Você está na rota de atendimento e está realizando um POST')
+    const atendimento = request.body;
+
+    Atendimento.adiciona(atendimento, response);
   })
 }
