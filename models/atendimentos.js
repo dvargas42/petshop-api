edb1c006de9d0802bbd029a223ad8713014c36c4
dvargas42 <dvargas.eng@gmail.com>
2021-07-03 01:36:05 -0300

    :tada: initial commit

---
+++ models/atendimentos.js
@@ -0,0 +1,48 @@
+const moment = require('moment')
+
+const conexao = require('../infraestrutura/conexao')
+
+class Atendimento {
+  adiciona(atendimento, res) {
+    const dataCriacao = moment().format ('YYYY-MM-DD HH:MM:SS')
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    const sql = 'INSERT INTO atendimentos SET ?';
+
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
+
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual'
+      }, 
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos cinco caracteres!'
+      }
+    ]
+
+    const erros = validacoes.filter(campo => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+
+    } else {
+      const atendimentoDatado = { ...atendimento, data, dataCriacao };
+  
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+  
+        } else {
+          res.status(201).json(resultados);
+        }
+      })
+    }
+  }
+}
+
+module.exports =  new Atendimento;
