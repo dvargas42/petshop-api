df30ebf6742644f38f3b3665b869d75e09de2a53
dvargas42 <dvargas.eng@gmail.com>
2021-07-03 01:35:40 -0300

    feat: implement data persistency

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const customExpress = require('./config/customExpress')
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = customExpress()
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro);
+    
+  } else {
+    console.log('Conectado com sucesso!');
 
-app.listen(3000, () => {
-  console.log('🚀 Servidor rodando na porta 3000!')
+    Tabelas.init(conexao);
+    const app = customExpress();
+
+    app.listen(3000, () => {
+      console.log('🚀 Servidor rodando na porta 3000!')
+    });
+  }
 });
