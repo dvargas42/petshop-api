f06ed45792c0d55101f2f46aef26b866bf5904f4
dvargas42 <dvargas.eng@gmail.com>
2021-07-08 16:41:24 -0300

    feat: implement file upload

--- models/pet.js
+++ models/pet.js
@@ -1,14 +1,23 @@
 const conexao = require("../infraestrutura/conexao");
+const uploadDeArquivos = require("../arquivos/uploadDeArquivos");
 
 class Pet {
   adiciona(pet, res) {
-    const query = "INSERT INTO pets SET ?";
+    const sql = "INSERT INTO pets SET ?";
 
-    conexao.query(query, pet, (erro) => {
+    uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
       if (erro) {
-        res.status(400).json(erro);
+        res.status(400).json({ erro });
       } else {
-        res.status(201).json(pet);
+        const novoPet = { nome: pet.nome, imagem: novoCaminho };
+
+        conexao.query(sql, pet, (erro) => {
+          if (erro) {
+            res.status(400).json(erro);
+          } else {
+            res.status(201).json(novoPet);
+          }
+        });
       }
     });
   }
