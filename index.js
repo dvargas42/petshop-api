6d5b008f1940c12f8819777e81235b4e6d5833ef
dvargas42 <dvargas.eng@gmail.com>
2021-07-10 18:19:11 -0300

    style: implement code styles

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const customExpress = require('./config/customExpress');
-const conexao = require('./infraestrutura/conexao');
-const Tabelas = require('./infraestrutura/tabelas');
+const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/database/conexao");
+const Tabelas = require("./infraestrutura/database/tabelas");
 
-conexao.connect(erro => {
+conexao.connect((erro) => {
   if (erro) {
     console.log(erro);
-    
   } else {
-    console.log('Conectado ao banco de dados com sucesso!');
+    console.log("Conectado ao banco de dados com sucesso!");
 
     Tabelas.init(conexao);
     const app = customExpress();
 
     app.listen(3000, () => {
-      console.log('🚀 Servidor rodando na porta 3000!')
+      console.log("🚀 Servidor rodando na porta 3000!");
     });
   }
 });
