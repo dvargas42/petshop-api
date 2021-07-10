0c5621e04cf038e4045fa1334671df99d4e4f923
dvargas42 <dvargas.eng@gmail.com>
2021-07-10 18:29:01 -0300

    refactor: implement repository logic and code styles

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,32 +1,36 @@
-const Atendimento = require('../models/atendimentos')
+const Atendimento = require("../models/atendimentos.model");
 
-module.exports = app => {
-  app.get('/atendimentos', (_, response) => {
-    Atendimento.lista(response)
+module.exports = (app) => {
+  app.get("/atendimentos", (_, response) => {
+    Atendimento.lista(response);
   });
 
-  app.get('/atendimentos/:id', (request, response) => {
+  app.get("/atendimentos/:id", (request, response) => {
     const id = parseInt(request.params.id);
 
     Atendimento.buscaPorId(id, response);
   });
 
-  app.post('/atendimentos', (request, response) => {
+  app.post("/atendimentos", (request, response) => {
     const atendimento = request.body;
 
-    Atendimento.adiciona(atendimento, response);
+    Atendimento.adiciona(atendimento)
+      .then((atendimentoCadastrado) => {
+        response.status(201).json(atendimentoCadastrado);
+      })
+      .catch((erros) => response.status(400).json(erros));
   });
 
-  app.patch('/atendimentos/:id', (request, response) => {
+  app.patch("/atendimentos/:id", (request, response) => {
     const id = parseInt(request.params.id);
     const valores = request.body;
 
     Atendimento.altera(id, valores, response);
   });
 
-  app.delete('/atendimentos/:id', (request, response) => {
+  app.delete("/atendimentos/:id", (request, response) => {
     const id = parseInt(request.params.id);
 
     Atendimento.deleta(id, response);
-  })
-}
+  });
+};
