253ee4da0965edf7d642a7acd65e3812a4e226a4
dvargas42 <dvargas.eng@gmail.com>
2021-07-12 02:09:31 -0300

    feat: create database queries

--- repositorios/atendimentos.repositorio.js
+++ repositorios/atendimentos.repositorio.js
@@ -5,6 +5,25 @@ class Atendimento {
     const sql = "INSERT INTO atendimentos SET ?";
     return query(sql, atendimento);
   }
+
+  lista() {
+    const sql = "SELECT * FROM atendimentos";
+    return query(sql);
+  }
+
+  buscaPorId(id) {
+    const sql = `SELECT * FROM atendimentos WHERE id=?`;
+    return query(sql, id);
+  }
+  altera(id, valores) {
+    const sql = "UPDATE atendimentos SET ? WHERE id=?";
+    return query(sql, [valores, id]);
+  }
+
+  deleta(id) {
+    const sql = "DELETE FROM atendimentos WHERE id=?";
+    return query(sql, id);
+  }
 }
 
 module.exports = new Atendimento();
