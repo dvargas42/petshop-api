9bf9c2db55096ea2a5a3ffe223486b73104f48be
dvargas42 <dvargas.eng@gmail.com>
2021-07-13 05:36:29 -0300

    refactor: change pets route

--- controllers/pets.js
+++ controllers/pets.js
@@ -4,6 +4,8 @@ module.exports = (app) => {
   app.post("/pets", (request, response) => {
     const pet = request.body;
 
-    Pet.adiciona(pet, response);
+    Pet.adiciona(pet)
+      .then((novoPet) => response.json(novoPet))
+      .catch((erros) => response.status(400).json(erros));
   });
 };
