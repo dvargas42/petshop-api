339ea0525440626aa258a15d4ee4daf7518cda55
dvargas42 <dvargas.eng@gmail.com>
2021-08-06 01:00:33 -0300

    feat: implement route update attendence

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -9,11 +9,18 @@ module.exports = app => {
     const id = parseInt(request.params.id);
 
     Atendimento.buscaPorId(id, response);
-  })
+  });
 
   app.post('/atendimentos', (request, response) => {
     const atendimento = request.body;
 
     Atendimento.adiciona(atendimento, response);
-  })
+  });
+
+  app.patch('/atendimentos/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    const valores = request.body;
+
+    Atendimento.altera(id, valores, response);
+  }) 
 }
